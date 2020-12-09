804d94847dbc85d0bb12d1878d431bbb1095070b
liguohao <liguohao_cn@qq.com>
2020-12-09 19:50:08 +0800

    完成RSA加解密功能

--- src/main/java/cn/liguohao/encrpt/RSAEncrptUtils.java
+++ src/main/java/cn/liguohao/encrpt/RSAEncrptUtils.java
@@ -1,12 +1,17 @@
 package cn.liguohao.encrpt;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 /**RSA加密工具类
@@ -86,6 +91,110 @@ public class RSAEncrptUtils {
         // 释放流
         publicKeyFileOutputStream.close();
         privateKeyFileOutputStream.close();
+    }
+
+    /**
+     * 用公钥加密文件
+     * @param publicKeyFile 公钥文件
+     * @param originalFile 源头文件
+     * @param encryptedFile 加密后文件
+     * @throws IOException 文件读取异常
+     */
+    public static void encrptFileByPublicKeyFile(File publicKeyFile,File originalFile, File encryptedFile) throws IOException {
+        // 文件存在判断
+        if(!originalFile.exists()) throw new FileNotFoundException(originalFile.getPath());
+
+        // 加密文件父目录不存在创建
+        if(!encryptedFile.getParentFile().exists()) encryptedFile.getParentFile().mkdirs();
+
+        // 获取公钥字节数组
+        byte[] publicKeyBytes = FileUtils.toByteArray(publicKeyFile);
+
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            // 获取公钥
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+            PublicKey publicKey = KeyFactory.getInstance(KEY_ALGORITHM).generatePublic(keySpec);
+
+            // 获取加密器
+            Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+
+            // 加密数据
+            byte[] encrptedBytes = cipher.doFinal(FileUtils.toByteArray(originalFile));
+
+            // 写入到文件
+            fileOutputStream = new FileOutputStream(encryptedFile);
+            fileOutputStream.write(encrptedBytes);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放流
+            fileOutputStream.close();
+        }
+
+    }
+
+
+    /**
+     * 根据私钥解密文件
+     * @param privateKeyFile 私钥文件
+     * @param encryptedFile 已经加密的文件
+     * @param decryptedFile 解密后的文件
+     * @throws IOException 文件读写异常
+     */
+    public static void decrptFileByPrivateKeyFile(File privateKeyFile, File encryptedFile, File decryptedFile) throws IOException {
+        // 文件存在判断
+        if(!encryptedFile.exists()) throw new FileNotFoundException(encryptedFile.getPath());
+
+        // 加密文件父目录不存在创建
+        if(!decryptedFile.getParentFile().exists()) decryptedFile.getParentFile().mkdirs();
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            // 构建私钥
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(FileUtils.toByteArray(privateKeyFile));
+            PrivateKey privateKey = KeyFactory.getInstance(KEY_ALGORITHM).generatePrivate(keySpec);
+
+            // 解密数据
+            Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE,privateKey);
+            byte[] decrptedBytes = cipher.doFinal(FileUtils.toByteArray(encryptedFile));
+
+            // 构建流写到文件
+            fileOutputStream = new FileOutputStream(decryptedFile);
+            fileOutputStream.write(decrptedBytes);
+
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放流
+            fileOutputStream.close();
+        }
+
+
     }
 
 }
--- src/test/java/cn/liguohao/encrpt/RSAEncrptUtilsTest.java
+++ src/test/java/cn/liguohao/encrpt/RSAEncrptUtilsTest.java
@@ -11,11 +11,55 @@ import java.io.IOException;
  */
 public class RSAEncrptUtilsTest {
 
+    /**
+     * 公钥文件
+     */
+    private File publicKeyFile = new File("R:\\test\\key\\publicKeyFile.pub");
+    /**
+     * 私钥文件
+     */
+    private File privateKeyFile = new File("R:\\test\\key\\privatekeyFile.pri");
+
+    /**
+     * 源文件
+      */
+    private File originalFile = new File("R:\\test\\key\\aeskey.key");
+
+    /**
+     * 加密后的文件
+     */
+    private File encrptedFile = new File("R:\\test\\encrypt\\aeskey.key.ikaros");
+
+    /**
+     * 解密后的文件
+     */
+    private File decrptedFile = new File("R:\\test\\decrypted\\aeskey.key");
+
+    /**
+     * 生成密钥
+     * @throws IOException 文件读写异常
+     */
     @Test
     public void generateKeyFile() throws IOException {
-        File publicKeyFile = new File("R:\\test\\key\\publicKeyFile.pub");
-        File privateKeyFile = new File("R:\\test\\key\\privatekeyFile.pri");
         RSAEncrptUtils.generateKeyFile(publicKeyFile,privateKeyFile);
+    }
+
+    /**
+     * 根据公钥加密
+     */
+    @Test
+    public void encrptFileByPublicKeyFile() throws IOException {
+        // 加密
+        RSAEncrptUtils.encrptFileByPublicKeyFile(publicKeyFile,originalFile,encrptedFile);
+    }
+
+    /**
+     * 解密文件
+     */
+    @Test
+    public void decrptFileByPrivateKeyFile() throws IOException {
+        // 解密
+        RSAEncrptUtils.decrptFileByPrivateKeyFile(privateKeyFile,encrptedFile,decrptedFile);
     }
 
 }
