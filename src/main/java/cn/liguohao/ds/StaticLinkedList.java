5f2df6e6c0206ef2b423c7cb27155fa37dd462b8
li-guohao <liguohao_cn@qq.com>
2021-03-30 11:59:59 +0800

    add note

--- src/main/java/cn/liguohao/ds/StaticLinkedList.java
+++ src/main/java/cn/liguohao/ds/StaticLinkedList.java
@@ -1,8 +1,15 @@
 package cn.liguohao.ds;
 
+import java.util.Scanner;
+
 /**
  * 静态链表
  * 数组实现的链式结构线性表
+ * 实现思路：
+ * <ul>
+ *     <li>两个顺序线性表，一个用于存放数据的数据表[data]，一个用于存放指针的指针域[cur]</li>
+ *     <li>节点的索引[index], 在数据域[data]对应的是链表节点的数据，在指针域[cur]对应的是链表节点的[next]指针</li>
+ * </ul>
  *
  * @author <a href="mailto:liguohao_cn@qq.com">liguohao_cn@qq.com</a>
  * @since 2021/3/28
@@ -11,16 +18,25 @@ public class StaticLinkedList<T> implements List<T>{
 
     /**
      * 数据域
+     * 用于存放链表节点的数据
+     * 相当于Node.data
      */
     private ArrayList<T> data = new ArrayList<T>();
 
     /**
      * 指针域
+     * 用于存放链表节点的指针
+     * 相当于Node.next
      */
-    private ArrayList<T> cur = new ArrayList<T>();
+    private ArrayList<Integer> cur = new ArrayList<Integer>();
+
+    /**
+     * 链表的元素个数
+     */
+    private int SIZE = 0;
 
     public StaticLinkedList(){
-        cur.set(0,data.get(0));
+
     }
 
 
@@ -41,11 +57,20 @@ public class StaticLinkedList<T> implements List<T>{
 
     @Override
     public void set(int index, T element) {
+        assert index>=0 : "list index is not negative";
+
+        data.set(index,element);
+        cur.set(index,++index);
+
+        ++SIZE;
 
     }
 
     @Override
     public void add(T element) {
+
+        data.add(element);
+        //cur.add();
 
     }
 
