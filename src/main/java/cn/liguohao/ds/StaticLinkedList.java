b4f4a4b415b9698995356b67b08e2798ffd43d87
li-guohao <liguohao_cn@qq.com>
2021-04-01 01:29:51 +0800

    添加了一些静态链表的实现，并未成功实现

--- src/main/java/cn/liguohao/ds/StaticLinkedList.java
+++ src/main/java/cn/liguohao/ds/StaticLinkedList.java
@@ -1,32 +1,77 @@
 package cn.liguohao.ds;
 
 /**
+ * 未实现
  * 静态链表
  * 数组实现的链式结构线性表
  *
  * @author <a href="mailto:liguohao_cn@qq.com">liguohao_cn@qq.com</a>
  * @since 2021/3/28
  */
-public class StaticLinkedList<T> implements List<T>{
+public class StaticLinkedList<T> implements List<T> {
 
     /**
-     * 数据域
+     * 节点数组
      */
-    private ArrayList<T> data = new ArrayList<T>();
+    private ArrayList<Node<T>> nodeArrayList = new ArrayList<Node<T>>();
 
     /**
-     * 指针域
+     * 链表元素个数
+     * 不包括头节点
      */
-    private ArrayList<T> cur = new ArrayList<T>();
+    private int size = 0;
 
-    public StaticLinkedList(){
-        cur.set(0,data.get(0));
+    /**
+     * 链表头节点
+     */
+    private Node<T> head = new Node<T>().setCur(1);
+
+    /**
+     * 节点
+     * @param <E> 节点存放数据的类型
+     */
+    class Node<E> {
+        /**
+         * 数据
+         */
+        E data;
+        /**
+         * 指针
+         */
+        int cur;
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> setData(E data) {
+            this.data = data;
+            return this;
+        }
+
+        public int getCur() {
+            return cur;
+        }
+
+        public Node<E> setCur(int cur) {
+            this.cur = cur;
+            return this;
+        }
     }
 
+    /**
+     * 初始化添加头节点
+     */
+    public StaticLinkedList() {
+        nodeArrayList.add(head);
+    }
 
+    /**
+     * @see List#isEmpty()
+     */
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -34,13 +79,24 @@ public class StaticLinkedList<T> implements List<T>{
 
     }
 
+    /**
+     * @see List#size()
+     */
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void set(int index, T element) {
+        assert index >= 0  : "索引不合法 ==> index="+index;
+
+        Node beforeNode = getBeforeIndexNode(index);
+        Node afterNode = nodeArrayList.get(beforeNode.cur);
+
+        Node<T> newNode = new Node<T>().setData(element).setCur(beforeNode.cur);
+        beforeNode.setCur(index);
+
 
     }
 
@@ -58,4 +114,18 @@ public class StaticLinkedList<T> implements List<T>{
     public T remove(int index) {
         return null;
     }
+
+    public Node getBeforeIndexNode(int index) {
+        return null;
+    }
+
+    public Node getIndexNode(int index) {
+        return null;
+    }
+
+    public Node getLastNode() {
+        return null;
+    }
+
+
 }
